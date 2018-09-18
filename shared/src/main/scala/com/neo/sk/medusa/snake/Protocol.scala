9ec043b55762ee143691706ab9f134d9ff610203
王希成 <wangxicheng@neotel.com.cn>
2018-09-18 16:03:28 +0800

    foodRate to 0.067

--- shared/src/main/scala/com/neo/sk/medusa/snake/Protocol.scala
+++ shared/src/main/scala/com/neo/sk/medusa/snake/Protocol.scala
@@ -65,7 +65,7 @@ object Protocol {
 
   val fSpeed = 10
   
-	val foodRate = 0.075 //尸体生成食物的倍率
+	val foodRate = 0.067 //尸体生成食物的倍率
 	
   val advanceFrame = 1 //客户端提前的帧数
   
--- shared/src/main/scala/com/neo/sk/medusa/snake/package.scala
+++ shared/src/main/scala/com/neo/sk/medusa/snake/package.scala
@@ -153,7 +153,7 @@ package object snake {
             (y + squareWide, y + (length + squareWide) * direction.y)
 
         (for {
-          xs <- (x - squareWide to x + squareWide)
+          xs <- x - squareWide to x + squareWide
           ys <- yStart to yEnd
         } yield {
           Point(xs, ys)
@@ -167,7 +167,7 @@ package object snake {
 
         (for {
           xs <- xStart to xEnd
-          ys <- (y - squareWide to y + squareWide)
+          ys <- y - squareWide to y + squareWide
         } yield {
           Point(xs, ys)
         }).toList
