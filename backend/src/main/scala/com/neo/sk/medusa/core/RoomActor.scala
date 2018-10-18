f8d405afa49dba08056c8e6e66f7c24f9cb119f2
zhangyuwei@neotel.com.cn <zhangyuwei@neotel.com.cn>
2018-10-18 14:47:34 +0800

    roomActor

--- backend/src/main/scala/com/neo/sk/medusa/core/RoomActor.scala
+++ backend/src/main/scala/com/neo/sk/medusa/core/RoomActor.scala
@@ -26,6 +26,8 @@ object RoomActor {
 
   case class NetTest(id: Long, createTime: Long) extends Command
 
+  case object KillRoom extends Command
+
   private case object Sync extends Command
 
   private case object BeginSync extends Command
@@ -41,48 +43,54 @@ object RoomActor {
         Behaviors.withTimers[Command] {
           implicit timer =>
             timer.startSingleTimer(TimerKey4SyncBegin, BeginSync, syncDelay.seconds)
-            idle(roomId, mutable.HashMap[Long,ActorRef[UserActor.Command]](),new GridOnServer(bound))
+            idle(roomId, 0, mutable.HashMap[Long,(ActorRef[UserActor.Command], String)](),new GridOnServer(bound))
         }
     }
   }
 
-  private def idle(roomId:Long, userMap:mutable.HashMap[Long, ActorRef[UserActor.Command]], grid:GridOnServer)
+  private def idle(roomId:Long, tickCount:Long,
+    userMap:mutable.HashMap[Long, (ActorRef[UserActor.Command], String)], grid:GridOnServer)
     (implicit timer: TimerScheduler[RoomActor.Command]):Behavior[Command]={
     Behaviors.receive[Command]{
       (ctx,msg)=>
         msg match {
-          case t:UserJoin =>
+          case t: UserJoin =>
             log.debug(s"room $roomId got a new player: ${t.userId}")
-            userMap.put(t.userId, t.userActor)
+            userMap.put(t.userId, (t.userActor, t.name))
             grid.addSnake(t.userId, t.name, roomId)
-            dispatchTo(t.userId, UserActor.DispatchMsg(Protocol.Id(t.userId)), userMap )
+            dispatchTo(t.userId, UserActor.DispatchMsg(Protocol.Id(t.userId)), userMap)
             dispatch(UserActor.DispatchMsg(Protocol.NewSnakeJoined(t.userId, t.name, roomId)), userMap)
             dispatch(UserActor.DispatchMsg(grid.getGridSyncData), userMap)
 
             Behaviors.same
 
-          case t:UserDead =>
+          case t: UserDead =>
             log.debug(s"room $roomId lost a player ${t.userId}")
             grid.removeSnake(t.userId)
-            dispatch(UserActor.DispatchMsg(Protocol.SnakeLeft(t.userId, t.name)),userMap)
+            dispatch(UserActor.DispatchMsg(Protocol.SnakeLeft(t.userId, t.name)), userMap)
             userMap.remove(t.userId)
             Behaviors.same
 
-          case t:Key =>
-//            if (t.keyCode == KeyEvent.VK_SPACE) {
-//              grid.addSnake(t.id,userMap.getOrElse(id, ( "Unknown",0))._1,roomId)
-//            } else {
-//              if(frame >= grid.frameCount) {
-//                grid.addActionWithFrame(id, keyCode, frame)
-//                dispatch(Protocol.SnakeAction(id, keyCode, frame),roomId)
-//              }else if(frame >= grid.frameCount-Protocol.savingFrame+Protocol.advanceFrame){
-//                grid.addActionWithFrame(id, keyCode, grid.frameCount)
-//                dispatchDistinct(id,Protocol.DistinctSnakeAction(keyCode, grid.frameCount, frame),Protocol.SnakeAction(id, keyCode, grid.frameCount), roomId)
-//                log.info(s"key delay: server: ${grid.frameCount} client: $frame")
-//              }else {
-//                log.info(s"key loss: server: ${grid.frameCount} client: $frame")
-//              }
-//            }
+          case t: Key =>
+            if (t.keyCode == KeyEvent.VK_SPACE) {
+              if (userMap.get(t.id).isEmpty) {
+                grid.addSnake(t.id, "unknown", roomId)
+              } else {
+                grid.addSnake(t.id, userMap(t.id)._2, roomId)
+              }
+            } else {
+              if (t.frame >= grid.frameCount) {
+                grid.addActionWithFrame(t.id, t.keyCode, t.frame)
+                dispatch(UserActor.DispatchMsg(Protocol.SnakeAction(t.id, t.keyCode, t.frame)), userMap)
+              } else if (t.frame >= grid.frameCount - Protocol.savingFrame + Protocol.advanceFrame) {
+                grid.addActionWithFrame(t.id, t.keyCode, grid.frameCount)
+                dispatchDistinct(t.id, UserActor.DispatchMsg(Protocol.DistinctSnakeAction(t.keyCode, grid.frameCount, t.frame)),
+                  UserActor.DispatchMsg(Protocol.SnakeAction(t.id, t.keyCode, grid.frameCount)), userMap)
+                log.info(s"key delay: server: ${grid.frameCount} client: ${t.frame}")
+              } else {
+                log.info(s"key loss: server: ${grid.frameCount} client: ${t.frame}")
+              }
+            }
             Behaviors.same
 
           case BeginSync =>
@@ -90,28 +98,72 @@ object RoomActor {
             Behaviors.same
 
           case Sync =>
+            val newTick = tickCount + 1
+            grid.update(false)
+            val feedApples = grid.getFeededApple
+            val eatenApples = grid.getEatenApples
+            val speedUpInfo = grid.getSpeedUpInfo
+            grid.resetFoodData()
+            if (grid.deadSnakeList.nonEmpty) {
+              dispatch(UserActor.DispatchMsg(Protocol.DeadList(grid.deadSnakeList.map(_.id))), userMap)
+              grid.deadSnakeList.foreach {
+                s =>
+                  dispatchTo(s.id, UserActor.DispatchMsg(Protocol.DeadInfo(s.name, s.length, s.kill, s.killer)), userMap)
+              }
+            }
+            grid.killMap.foreach {
+              g =>
+                dispatchTo(g._1, UserActor.DispatchMsg(Protocol.KillList(g._2)), userMap)
+            }
+            if (tickCount % 20 == 5) {
+              val GridSyncData = grid.getGridSyncData
+              dispatch(UserActor.DispatchMsg(GridSyncData), userMap)
+            } else {
+              if (feedApples.nonEmpty) {
+                dispatch(UserActor.DispatchMsg(Protocol.FeedApples(feedApples)), userMap)
+              }
+              if (eatenApples.nonEmpty) {
+                dispatch(UserActor.DispatchMsg(Protocol.EatApples(eatenApples.map(r => EatFoodInfo(r._1, r._2)).toList)), userMap)
+              }
+              if (speedUpInfo.nonEmpty) {
+                dispatch(UserActor.DispatchMsg(Protocol.SpeedUp(speedUpInfo)), userMap)
+              }
+            }
+            if (tickCount % 20 == 1) {
+              dispatch(UserActor.DispatchMsg(Protocol.Ranks(grid.currentRank, grid.historyRankList)), userMap)
+            }
+            idle(roomId, newTick, userMap, grid)
 
+          case NetTest(id, createTime) =>
+            dispatchTo(id, UserActor.DispatchMsg(Protocol.NetDelayTest(createTime)), userMap)
             Behaviors.same
 
+          case KillRoom =>
+            Behaviors.stopped
+
+
+          case x =>
+            log.warn(s"got unknown msg: $x")
+            Behaviors.same
         }
 
     }
 
-    def dispatchTo(id: Long, gameOutPut: UserActor.DispatchMsg, userMap:mutable.HashMap[Long, ActorRef[UserActor.Command]]): Unit = {
-      userMap.get(id).foreach { ref => ref ! gameOutPut }
+    def dispatchTo(id: Long, gameOutPut: UserActor.DispatchMsg, userMap:mutable.HashMap[Long, (ActorRef[UserActor.Command], String)]): Unit = {
+      userMap.get(id).foreach { t => t._1 ! gameOutPut }
     }
 
-    def dispatch(gameOutPut: UserActor.DispatchMsg, userMap:mutable.HashMap[Long, ActorRef[UserActor.Command]]) = {
-      userMap.values.foreach { ref => ref ! gameOutPut }
+    def dispatch(gameOutPut: UserActor.DispatchMsg, userMap:mutable.HashMap[Long, (ActorRef[UserActor.Command], String)]) = {
+      userMap.values.foreach { t => t._1 ! gameOutPut }
     }
 
-    def dispatchDistinct(distinctId:Long, distinctGameOutPut:UserActor.DispatchMsg, gameOutPut: UserActor.DispatchMsg, userMap:mutable.HashMap[Long, ActorRef[UserActor.Command]]): Unit ={
+    def dispatchDistinct(distinctId:Long, distinctGameOutPut:UserActor.DispatchMsg, gameOutPut: UserActor.DispatchMsg, userMap:mutable.HashMap[Long, (ActorRef[UserActor.Command], String)]): Unit ={
       userMap.foreach {
-        case (id, ref)  =>
+        case (id, t) =>
           if(id != distinctId){
-            ref ! gameOutPut
+            t._1 ! gameOutPut
           }else{
-            ref ! distinctGameOutPut
+            t._1 ! distinctGameOutPut
           }
         case _ =>
       }
