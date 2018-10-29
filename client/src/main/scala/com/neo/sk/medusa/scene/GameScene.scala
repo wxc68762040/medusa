899a1ccfc5c9c1c528d43d4d6a6d5ae1076e1aa6
王希成 <wangxicheng@neotel.com.cn>
2018-10-29 17:40:50 +0800

    close window

--- client/src/main/scala/com/neo/sk/medusa/scene/GameScene.scala
+++ client/src/main/scala/com/neo/sk/medusa/scene/GameScene.scala
@@ -30,16 +30,16 @@ class GameScene() {
 
 	val widthMap = 250
 	val heightMap = 600
-	val viewWidth =1200
-	val viewHeight =600
+	val viewWidth = 1200
+	val viewHeight = 600
 	val infoWidth = 250
 	val infoHeight = 300
 	val group = new Group
 	val mapCanvas = new Canvas(widthMap, heightMap)
 	val viewCanvas = new Canvas(viewWidth, viewHeight)
   val infoCanvas = new Canvas(infoWidth, infoHeight)
-	infoCanvas.setStyle("z-index = 100")
-  mapCanvas.setStyle("z-index = 120")
+	infoCanvas.setStyle("z-index: 100")
+  mapCanvas.setStyle("z-index: 120")
 
 	val scene = new Scene(group)
 
