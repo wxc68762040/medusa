de547afe938fd80ef70875bde8f65157cd0dbdcd
王希成 <wangxicheng@neotel.com.cn>
2018-11-02 15:57:32 +0800

    getRecordPlayerList api

--- backend/src/main/scala/com/neo/sk/medusa/protocol/RecordApiProtocol.scala
+++ backend/src/main/scala/com/neo/sk/medusa/protocol/RecordApiProtocol.scala
@@ -1,87 +1,107 @@
 package com.neo.sk.medusa.protocol
 
 object RecordApiProtocol {
-//全量获取比赛录像列表
+  
+  sealed trait Request
+  sealed trait Response{
+    val errCode: Int
+    val msg: String
+  }
+  
+  //全量获取比赛录像列表
   case class RecordListReq(
-                          lastRecordId:Long,
-                          count:Int
-                          )
+    lastRecordId: Long,
+    count: Int
+  ) extends Request
 
-//按时间获取比赛录像列表
+  //按时间获取比赛录像列表
   case class RecordListByTimeReq(
-                                startTime:Long,
-                                endTime:Long,
-                                lastRecordId:Long,
-                                count:Int
-                                )
-//筛选指定玩家比赛录像列表
+    startTime: Long,
+    endTime: Long,
+    lastRecordId: Long,
+    count: Int
+  ) extends Request
+
+  //筛选指定玩家比赛录像列表
   case class RecordListByPlayerReq(
-                                  playerId:String,
-                                  lastRecordId:Long,
-                                  count:Int
-                                  )
-//获取比赛录像的返回格式
+    playerId: String,
+    lastRecordId: Long,
+    count: Int
+  ) extends Request
+
+  //获取比赛录像的返回格式
   case class Record(
-                         recordId:Long,
-                         roomId:Long,
-                         startTime:Long,
-                         endTime:Long,
-                         userCounts:Int,
-                         userList:Seq[String]
-                         )
+    recordId: Long,
+    roomId: Long,
+    startTime: Long,
+    endTime: Long,
+    userCounts: Int,
+    userList: Seq[String]
+  )
+  
   case class RecordResponse(
-                                 data:Option[List[Record]],
-                                 errCode:Int = 0,
-                                 msg:String = "ok"
-                               )
+    data: Option[List[Record]],
+    errCode: Int = 0,
+    msg: String = "ok"
+  ) extends Response
 
-
-
-//下载录像
-  case class DownloadRecordFile(
-                               recordId:Long
-                               )
+  
+  //下载录像
+  case class DownloadRecordFileReq(
+    recordId: Long
+  ) extends Request
 
 
   //获取录像内玩家列表
-
-  case class RecordPlayerList(
-                             recordId:Long,
-                             playerId:String
-                             )
+  case class RecordPlayerListReq(
+    recordId: Long,
+    playerId: String
+  ) extends Request
+  
+  case class RecordExistTime(
+    startFrame: Long,
+    endFrame: Long
+  )
+  
   case class UserInfoInRecord(
-                             playId:String,
-                             nickName:String
-                             )
-
-  case class RecordPlayerListResponse(
-                                     data:Option[List[UserInfoInRecord]],
-                                     errCode:Int = 0,
-                                     msg:String = "ok"
-                                     )
-//获取录像进度
-
-  case class GetRecordFrameReq(
-    recordId:Long,
-    playerId:String
+    playerId: String,
+    nickName: String,
+    existTime: List[RecordExistTime]
   )
 
+  case class RecordPlayerList(
+    totalFrame: Long,
+    playerList: Option[List[UserInfoInRecord]]
+  )
+  
+  case class RecordPlayerListResponse(
+    data: RecordPlayerList,
+    errCode:Int = 0,
+    msg:String = "ok"
+  ) extends Response
+
+  //获取录像进度
+  case class GetRecordFrameReq(
+    recordId: Long,
+    playerId: String
+  ) extends Request
+
   case class FrameInfo(
-    frame:Int,
-    frameNum:Long
+    frame: Int,
+    frameNum: Long
   )
 
   case class GetRecordFrameRsp(
-    data:FrameInfo,
-    errCode:Int = 0,
-    msg:String = "ok"
-  )
+    data: FrameInfo,
+    errCode: Int = 0,
+    msg: String = "ok"
+  ) extends Response
 
-//失败返回
+
+  //失败返回
   case class GetResourceError(
-                               errCode:Int,
-                               msg:String
-                             )
-
+    errCode: Int,
+    msg: String
+  ) extends Response
 
 }
