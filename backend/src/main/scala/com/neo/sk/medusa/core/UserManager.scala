5123f1ac9851662ad29288a1e9b734f18dc998d2
王希成 <wangxicheng@neotel.com.cn>
2018-11-06 11:29:56 +0800

    forceRenewUserActor

--- backend/src/main/scala/com/neo/sk/medusa/core/UserManager.scala
+++ backend/src/main/scala/com/neo/sk/medusa/core/UserManager.scala
@@ -61,11 +61,14 @@ object UserManager {
       (ctx, msg) =>
         msg match {
           case GetWebSocketFlow(playerId, playerName, roomId, replyTo) =>
-            if(allUser.get(playerId).isDefined){
-              getUserActor(ctx, playerId, playerName) ! UserActor.KillSelf
-              allUser.remove(playerId)
-            }
-            val user = forceRenewUserActor(ctx, playerId, playerName)
+            val user =
+              if(allUser.get(playerId).isDefined){
+                getUserActor(ctx, playerId, playerName) ! UserActor.KillSelf
+                allUser.remove(playerId)
+                forceRenewUserActor(ctx, playerId, playerName)
+              } else {
+                getUserActor(ctx, playerId, playerName)
+              }
             allUser.put(playerId, user)
             if (userRoomMap.get(playerId).nonEmpty) {
               userRoomMap.update(playerId, (roomId, playerName))
